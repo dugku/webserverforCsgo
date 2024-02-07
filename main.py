1511cb23d17fa06ae0d489c5708ab38c1f06f9af
mpurtle <mpurtle@uark.edu>
2024-02-07 12:20:44 -0600

    Added soemstuff need to test later

--- main.py
+++ main.py
@@ -13,7 +13,10 @@ class Matches(db.Model):
     playerscoreboard = db.relationship('playerScoreMatch', backref='match' ,lazy=True)
 
 class Rounds(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
     econTeamA = db.Column(db.Integer)
+    scoreA = db.Column(db.Integer)
+    scoreB = db.Column(db.Integer)
     econTeamB = db.Column(db.Integer)
     bombPlanted = db.Column(db.Boolean)
     playerPlanted = db.Column(db.String(25))
@@ -22,7 +25,9 @@ class Rounds(db.Model):
     WhatMatch = db.Column(db.Integer, db.ForeignKey('matches.id'), nullable = False)
 
 class playerScoreMatch(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
     steamId = db.Column(db.Integer, unique = True)
+    impact = db.Column(db.Float)
     userName = db.Column(db.String(30))
     kills = db.Column(db.Integer)
     deaths = db.Column(db.Integer)
@@ -42,28 +47,31 @@ class playerScoreMatch(db.Model):
     tradekills = db.Column(db.Integer)
     tradedeaths = db.Column(db.Integer)
 
-class players(db.Model):
-    pass
-
 @app.route("/MatchData", methods=["POST"])
 def get_match():
 
     Math_json = request.get_json()
 
-    MapPlayed = Math_json["Map"]
-   
-    WhoPlayedWho = Math_json["WhoVsWho"]
+    new_match = Matches(
+        Map=Math_json["Map"], 
+        TeamsPlayed=Math_json["WhovsWho"]
+    )
 
+    new_round = Rounds(
+        econTeamA = Math_json.Rounds.EconA,
+        econTeamB = Math_json.Rounds["EconB"],
+        bombPlanted = Math_json.Rounds["BombPlanted"],
+        #playerPlanted = Math_json["PLayer"]
+        typeOfBuyTeamA = Math_json.Rounds["TypeofBuyA"],
+        typeofBuyTeamB = Math_json.Rounds["TypeofBuyB"],
+        scoreA = Math_json.Rounds["ScoreA"],
+        scoreB = Math_json.Rounds["ScoreB"],
+    )
 
-    for i in Math_json["Rounds"]:
+    new_round.match = new_round
+    print(new_match)
+    print(new_round)
 
-        if i["EconA"] == 0:
-            continue
-
-        EconA = i["EconA"]
-        EconB = i["EconB"]
-        TypeOfBuyA = i["TypeOfBuyA"]
-        TypeOfBuyB = i["TypeOfBuyB"]
 
     return jsonify(Math_json)
 
