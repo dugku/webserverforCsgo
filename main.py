6c99ddd5bf7da41761c80c5a4f5cfcc7d9785b35
mpurtle <mpurtle@uark.edu>
2024-02-10 04:11:50 -0600

    Did some stuff

--- main.py
+++ main.py
@@ -9,8 +9,8 @@ class Matches(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     Map = db.Column(db.String(35))
     TeamsPlayed = db.Column(db.String(40))
-    roundsPlayed = db.relationship('Rounds', backref='match',  lazy=True)
-    playerscoreboard = db.relationship('playerScoreMatch', backref='match' ,lazy=True)
+    roundsPlayed = db.relationship('Rounds', back_populates='match',  lazy=True)
+    playerscoreboard = db.relationship('playerScoreMatch',back_populates='match' ,lazy=True)
 
 class Rounds(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -22,7 +22,12 @@ class Rounds(db.Model):
     playerPlanted = db.Column(db.String(25))
     typeOfBuyTeamA = db.Column(db.String(15))
     typeofBuyTeamB = db.Column(db.String(15))
-    WhatMatch = db.Column(db.Integer, db.ForeignKey('matches.id'), nullable = False)
+    sidewon = db.Column(db.String(25))
+    teamAname = db.Column(db.String(30))
+    teamBname = db.Column(db.String(30))
+    match_id = db.Column(db.Integer, db.ForeignKey('matches.id'), nullable=False)
+    match = db.relationship('Matches', back_populates='roundsPlayed')
+
 
 class playerScoreMatch(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -46,6 +51,8 @@ class playerScoreMatch(db.Model):
     totaldmg = db.Column(db.Integer)
     tradekills = db.Column(db.Integer)
     tradedeaths = db.Column(db.Integer)
+    match_id = db.Column(db.Integer, db.ForeignKey('matches.id'), nullable=False)
+    match = db.relationship("Matches", back_populates='playerscoreboard')
 
 @app.route("/MatchData", methods=["POST"])
 def get_match():
@@ -53,24 +60,37 @@ def get_match():
     Math_json = request.get_json()
 
     new_match = Matches(
-        Map=Math_json["Map"], 
-        TeamsPlayed=Math_json["WhovsWho"]
+        Map=Math_json["Match"]['Map'], 
+        TeamsPlayed=Math_json["Match"]['WhoVsWho']
     )
 
-    new_round = Rounds(
-        econTeamA = Math_json.Rounds.EconA,
-        econTeamB = Math_json.Rounds["EconB"],
-        bombPlanted = Math_json.Rounds["BombPlanted"],
-        #playerPlanted = Math_json["PLayer"]
-        typeOfBuyTeamA = Math_json.Rounds["TypeofBuyA"],
-        typeofBuyTeamB = Math_json.Rounds["TypeofBuyB"],
-        scoreA = Math_json.Rounds["ScoreA"],
-        scoreB = Math_json.Rounds["ScoreB"],
-    )
 
-    new_round.match = new_round
-    print(new_match)
-    print(new_round)
+    rounds = Math_json["Match"]["Rounds"]
+
+    for i in rounds:
+
+        if i["ScoreA"] == 0 and i["ScoreB"] == 0:
+            continue 
+
+        new_round = Rounds(
+        econTeamA = i["EconA"],
+        econTeamB = i["EconB"],
+        bombPlanted = i["BombPlanted"],
+        typeOfBuyTeamA = i["TypeofBuyA"],
+        typeofBuyTeamB = i["TypeofBuyB"],
+        sidewon = i["SideWon"],
+        teamAname = i["TeamNameA"],
+        teamBname = i["TeamNameB"],
+        scoreA = i["ScoreA"],
+        scoreB = i["ScoreB"],
+        
+        )
+
+        db.session.add(new_round)
+
+    
+
+    for x in 
 
 
     return jsonify(Math_json)
